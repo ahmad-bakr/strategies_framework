c41b345914ed097a2e84ef25efe77ac7c2b58fc0
Ahmad Bakr <ahmad.m.bakr@gmail.com>
2015-10-12 22:13:05 +0900

    adding the framework skeleton, the strategy interface and the workers containers

--- src/com/strategy/containers/StrategiesContainer.java
+++ src/com/strategy/containers/StrategiesContainer.java
@@ -8,7 +8,7 @@ import java.util.Map;
 import com.strategy.containers.exceptions.StrategyAlreadyExistsException;
 
 /**
- * Strategies container
+ * Strategies container. Manages all Strategies and their workers.
  * @author ahmad.bakr
  *
  */
